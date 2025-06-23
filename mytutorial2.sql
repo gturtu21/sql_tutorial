2bc3200fc0926c097ef7a6bbc41409d1d328359f
gturtu21 <giorgio.turtu2@unibo.it>
2025-06-23 22:36:40 +0200

    Tutorial updates

--- mytutorial2.sql
+++ mytutorial2.sql
@@ -48,3 +48,34 @@ JOIN departments d ON e.department = d.name;
 SELECT name, salary
 FROM employees
 WHERE salary > (SELECT AVG(SALARY) FROM employees);
+
+-- CTE (Common Table Expression)
+WITH high_paid AS (
+    SELECT * FROM employees WHERE salary > 80000
+)
+SELECT * FROM high_paid;
+
+-- window functions
+--Use window functions when you want to analyze data across rows without grouping.
+--Combine them with PARTITION BY (optional) to define groups.
+--Add ORDER BY (optional) to define order within the group.
+
+SELECT name, department, salary,
+    RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS rank_in_dept
+FROM employees;
+
+-- Constraints and Indexes
+--Yes, indexing columns like department is very common and recommended when:
+--The column is queried frequently.
+--The table is medium to large in size.
+--You care about performance and scalability.
+
+CREATE INDEX idx_department ON employees(department);
+
+-- transactions
+BEGIN;
+UPDATE employees SET salary = salary * 1.1 WHERE department = 'Sales';
+ROLLBACK; -- Undoes all changes made since BEGIN;, leaving the table unchanged.(Use COMMIT; instead if you want to make the changes permanent.)
+
+-- bonus: String functions
+SELECT name, UPPER(name) AS name_upper, LENGTH(name) AS name_length FROM employees;
